2ad2a638088938d6ded3b89dbc803614f63ed183
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-06-25 20:46:14 -0400

    Another coverage bug.

--- Coverage.ps1
+++ Coverage.ps1
@@ -1,3 +1,3 @@
 $testProjectLocations = @('test/UnitTests', 'test/UnitTests.Streams')
 $outputLocation = 'testResults'
-iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/StephenCleary/BuildTools/86755979d99fed10421697f89317348ebec8edf4/Coverage.ps1'))
+iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/StephenCleary/BuildTools/640c77d28ced405458b067de901bc611db60b880/Coverage.ps1'))
